c6f3e8e183843358a4f36d20e75875490d8fb8cb
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-28 09:13:17 +0545

    Updated feature X or fixed bug Y

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render,HttpResponse
-from .import RegisterForm
+from django.shortcuts import render,HttpResponse, redirect
+from . forms import RegisterForm
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
 
 def register_view(request):
-    form = RegisterForm()
-    context ={
-        'form':form
-    }
-    return render(request, "register.html")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    
+    return render(request, "register.html", {'form':form})
 
 def login_view(request):
     return render(request, "login.html")
