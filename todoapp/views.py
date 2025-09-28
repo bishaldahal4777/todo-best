67a16c0e4c06199e7017c3f36a0c3d2684509961
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-28 08:54:37 +0545

    Updated feature X or fixed bug Y

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render,HttpResponse
+from .import RegisterForm
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
 
 def register_view(request):
+    form = RegisterForm()
+    context ={
+        'form':form
+    }
     return render(request, "register.html")
 
 def login_view(request):
