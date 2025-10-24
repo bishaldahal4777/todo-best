039386b73ca814218df4225fa7846f9c95eeadfa
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-24 18:12:39 +0545

    new commit

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('login/', views.login_view, name="login"), 
     path('logout/', views.logout_view, name="logout"), 
 
-    path('delete/<int:id>/', views.delete_task, name="deleted"),
 
 ]
